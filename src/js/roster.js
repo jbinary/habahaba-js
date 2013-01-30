a42d5a5a0cf6e430e1dcce32f691c3ba460af437
Binary <binary@jrudevels.org>
2013-01-30 20:11:25 +0700

    roster edit events handling

--- src/js/roster.js
+++ src/js/roster.js
@@ -47,33 +47,57 @@
         };
         this.roster = new jslix.roster(this.dispatcher);
         this.roster.signals.got.add(this.got_roster);
+        this.roster.signals.updated.add(this.roster_updated);
         this.dispatcher.addHandler(presence, this, 'client.roster');
     }
 
-    Client.prototype.got_roster = function(roster) {
-        for (var i=0; i<roster.items.length; i++) {
-            var item = roster.items[i];
-            var groups = [];
-            for (var gi=0; gi<item.groups.length; gi++) {
-                var model = new Model('.roster.groups');
-                var group = model.get({name: item.groups[gi]});
-                if (!group) {
-                    group = model.new();
-                    group.name = item.groups[gi];
-                    group.set();
-                }
-                groups.push(group.pk);
+    var _prepare_roster_item = function(item, old_item) {
+        var groups = [];
+        for (var gi=0; gi<item.groups.length; gi++) {
+            var model = new Model('.roster.groups');
+            var group = model.get({name: item.groups[gi]});
+            if (!group) {
+                group = model.new();
+                group.name = item.groups[gi];
+                group.set();
             }
-            if (!item.groups.length) {
-                var group = new Model('.roster.groups');
-                group = group.get({special_group: 'undefined'})
-                groups.push(group.pk);
-            }
-            var model = new Model('.roster.items').new();
-            model.fromDocument(item);
+            groups.push(group.pk);
+        }
+        if (!item.groups.length) {
+            var group = new Model('.roster.groups');
+            group = group.get({special_group: 'undefined'})
+            groups.push(group.pk);
+        }
+        model = old_item || new Model('.roster.items').new();
+        model.fromDocument(item);
+        if (!old_item)
             model.presences = [];
-            model.groups = groups;
-            model.set();
+        else
+            model.presences = old_item.presences;
+        model.groups = groups;
+        model.set();
+    }
+
+    Client.prototype.roster_updated = function(items) {
+        var all_items = new Model('.roster.items').getAll();
+        for (var i=0; i<items.length; i++) {
+            var item = items[i];
+            var the_item = all_items.filter(function(citem) {
+                return (citem.jid.getBareJID() == item.jid.getBareJID())
+            });
+            if (the_item.length)
+                the_item = the_item[0]
+            else {
+                the_item = undefined;
+            }
+            _prepare_roster_item(item, the_item);
+        }
+    }
+
+    Client.prototype.got_roster = function(items) {
+        for (var i=0; i<items.length; i++) {
+            var item = items[i];
+            _prepare_roster_item(item);
         }
     }
 })();
