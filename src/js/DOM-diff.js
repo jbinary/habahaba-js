f95aa528a5627af1336e27ca9facf0b68e6797b0
Binary <binary@jrudevels.org>
2013-02-05 18:17:09 +0700

    update world with diffs, allowing hide and show animation

---
+++ src/js/DOM-diff.js
@@ -0,0 +1,115 @@
+/**
+ * Perform a difference check between two DOM elements
+ *
+ * (c) Mike "Pomax" Kamermans, but you can freely use it.
+ */
+var DOMdiff = (function() {
+
+  var diffObject = {};
+
+  /**
+   * DOM element comparator.
+   *
+   * The fact that JavaScript doesn't have this
+   * built into the DOM API is lamentable.
+   *
+   * return -1 if "plain" not equal,
+   *         0 if equal,
+   *        or a <number>[] representing the
+   *        route in this element to a diff.
+   *
+   * FIXME: as long as the tagname stays the same, outerchange (class, etc) should be a modification, not top diff.
+   *
+   */
+  var equal = function equal(e1, e2/*, after*/) {
+
+    // first: if this element is a previous route's problem
+    // point, we're going to TOTALLY ignore it and pretend it's
+    // fine, so that we can find further problems.
+    /*var soffset = (after && after.length!==0 ? after.splice(0,1)[0] : 0);
+    if(soffset === -1) {
+      return 0;
+    }*/
+
+    // different element (1)?
+    if(e1.nodeType !== e2.nodeType) {
+      return -1;
+    }
+
+    // shortcut handling for text?
+    if(e1.nodeType===3 && e2.nodeType===3) {
+      if(e1.textContent.trim() != e2.textContent.trim()) {
+        return -1;
+      }
+      return 0;
+    }
+
+    // different element (2)?
+    if(e1.nodeName.toLowerCase() !== e2.nodeName.toLowerCase()) {
+      return -1;
+    }
+
+    // different content?
+    var l1 = $(e1).children().not('*[removed]').size();
+    var l2 = $(e2).children().not('*[removed]').size();
+    if(l1 != l2) {
+      return -1;
+    }
+
+    // different attributes?
+    var attrs = ["id",     // ids MUST be identical, nice and simple
+                 //"style",  // this one's tricky, and I don't want to write a full CSS parser right now. FIXME: later
+                 "class",  // this one's less tricky, but still requires split/sort comparison. FIXME: later
+                 "type",
+                 "value",
+                 "href",
+                 "src",
+                 "rel",
+                 "__more__attributes__here__"],
+        a, last = attrs.length,
+        attr, a1, a2;
+
+    for(a=0; a<last; a++) {
+      attr = attrs[a];
+      a1 = e1.getAttribute(attr);
+      a2 = e2.getAttribute(attr);
+      if(a1==a2 || (!a1 && a2=="") || (!a2 && a1=="")) continue;
+      return -1;
+    }
+
+    // Different child node list?
+    // Find where the first difference is
+    var i1 = 0, last1 = e1.childNodes.length, eq, ret;
+    var i2 = 0, last2 = e2.childNodes.length;
+    while (i1 < last1 && i2 < last2) {
+      // recurse to see if these children differ
+      var node1 = e1.childNodes[i1];
+      if (node1.hasAttribute && node1.hasAttribute('removed')) {
+        i1++;
+        continue;
+      }
+      var node2 = e2.childNodes[i2];
+      if (node2.hasAttribute && node2.hasAttribute('removed')) {
+        i2++;
+        continue;
+      }
+      eq = equal(node1, node2/*, after*/);
+      if(eq !== 0)
+      {
+        // (first) difference found. "eq" will indicate
+        // which childNodes position the diff is found at.
+        return [[i1, i2]].concat(eq);
+      }
+      i1++;
+      i2++;
+    }
+
+    // nothing left to fail on - consider
+    // these two elements equal.
+    return 0;
+  };
+
+  diffObject.equal = equal;
+
+  return diffObject;
+}());
--- src/js/main.js
+++ src/js/main.js
@@ -15,15 +15,159 @@
         var dispatcher = _modelEngine.dispatcher;
         var Model = _modelEngine.Model;
 
-        dispatcher.bind('world:changed', function() {
-            var rendered = dispatcher.render(new Model('__main__'));
-            var scrollTop = document.getElementById('roster');
-            if (scrollTop)
-                scrollTop = scrollTop.scrollTop;
-            $('body').html(rendered);
-            if (scrollTop)
-                document.getElementById('roster').scrollTop = scrollTop;
-        });
+var _moveNode = function(parent, node, i) {
+    if (i == parent.childNodes.length - 1) {
+        parent.appendChild(node);
+    } else {
+        parent.insertBefore(node, parent.childNodes[i+1]);
+    }
+}
+
+var get_event_handler = function(node, event_name) {
+    var handler;
+    try {
+        handler = new Function(node.getAttribute(event_name));
+    } catch (e) {} // TODO: logging?
+    return handler;
+}
+
+var update_world = function(rendered) {
+    var replace = function(child) {
+        var work_el = child;
+        while (!work_el.getAttribute || 
+                !work_el.getAttribute('id')) {
+            work_el = work_el.parentElement;
+        }
+        var id = work_el.getAttribute('id');
+        var new_el = wrapper.getElementById(id);
+        if (new_el) {
+            $(work_el).replaceWith($(new_el).clone());
+            return true;
+        }
+    }
+
+    var wrapper = document.implementation.createHTMLDocument();
+    wrapper.body.innerHTML = rendered;
+    rendered = wrapper.body;
+
+    var d1 = rendered.firstChild,
+        d2 = $('body')[0].firstChild;
+    while (true) {
+        var path = DOMdiff.equal(d1, d2);
+        if (!path) break;
+        var e1 = d1, e2 = d2;
+        for (var p=0; p<path.length - 1; p++) {
+            var c = path[p];
+            var e1 = e1.childNodes[c[0]];
+            var e2 = e2.childNodes[c[1]];
+        }
+        if (e1.childNodes.length == e2.childNodes.length && 
+            e1.childNodes.length == 0) {
+            $(e2).replaceWith($(e1).clone());
+            continue;
+        }
+
+        // Check if two elements were swapped under the same parent
+        var id1 = e1.getAttribute('id');
+        var id2 = e2.getAttribute('id');
+        if (id1 && id2 && id1 != id2) {
+            var _e1 = document.getElementById(id1);
+            var _e2 = wrapper.getElementById(id2);
+            var i1 = $(e1.parentElement).children().index($(e1));
+            var i2 = $(e1.parentElement).children().index($(_e2));
+            if (i1 >= 0 && i2 >= 0) {
+                // They are actually swapped!
+                var parent = e2.parentElement;
+                _moveNode(parent, _e1, i1);
+                _moveNode(parent, e2, i2);
+            }
+            continue;
+        }
+
+        for (var c=0, len=e1.childNodes.length; c<len; c++) {
+            var child = e1.childNodes[c];
+            var _child = e2.childNodes[c];
+            if (_child) {
+                if (_child.hasAttribute('removed')) {
+                    $(_child).stop(true, true);
+                    $(_child).show();
+                    _child.removeAttribute('removed');
+                }
+                var _p = DOMdiff.equal(child, _child);
+                if (_p === 0) continue;
+            }
+            var id = child.getAttribute('id');
+            if (!id) {
+                replace(child);
+                break;
+            }
+            var old_el = document.getElementById(id);
+            if (old_el && old_el.parent != e2) {
+                // Move TODO
+            } else if (!old_el) {
+                // Add
+                var cloned = child.cloneNode(true);
+                var prev, next = undefined;
+                if (len != 1 && c>0) {
+                    prev = e1.childNodes[c-1].getAttribute('id');
+                    prev = document.getElementById(prev);
+                } else if (len != 1 && c<len-1) {
+                    next = e1.childNodes[c+1].getAttribute('id');
+                    next = document.getElementById(next);
+                }
+                if (!next && !prev) {
+                    var showed = e2.appendChild(cloned);
+                } else if (next) {
+                    var showed = $(cloned).insertBefore($(next));
+                } else {
+                    var showed = $(cloned).insertAfter($(prev));
+                }
+                var handler = get_event_handler(cloned, 'onshow');
+                if (handler) {
+                    handler.apply(cloned);
+                }
+            } else {
+                console.log('Something went wrong');
+                break;
+            }
+        }
+        for (var c=0, len=e2.childNodes.length; c<len; c++) {
+            var child = e2.childNodes[c];
+            var _child = e1.childNodes[c];
+            if (_child) {
+                var _p = DOMdiff.equal(child, _child);
+                if (_p === 0) continue;
+            }
+            var f = replace(child);
+            // TODO: optimisation: don't look through elements
+            // that were processed in the previous loop
+            if (!f) {
+                // Remove
+                child.setAttribute('removed', true);
+                var handler = get_event_handler(child, 'onhide');
+                if (handler) handler.apply(child);
+
+                $(child).promise().done(function() {
+                    if (this[0].hasAttribute('removed')) {
+                        try {
+                            this[0].parentElement.removeChild(this[0]);
+                        } catch (e) {}
+                    }
+                });
+            }
+        }
+    }
+}
+
+dispatcher.bind('world:changed', function() {
+    var rendered = dispatcher.render(new Model('__main__'));
+
+    if ($('#wrapper').size() == 0) {
+        $('body').html(rendered);
+    } else {
+        update_world(rendered);
+    }
+});
 
         dispatcher.start();
         window.Model = Model;
