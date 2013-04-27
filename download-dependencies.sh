5fdb4e000d9e6dfe6bc5dc03691b65e1363026f8
Binary <binary@jrudevels.org>
2013-04-27 19:00:48 +0700

    use local version of jquery to be able to run tests

--- download-dependencies.sh
+++ download-dependencies.sh
@@ -4,5 +4,6 @@ DIR=libs
 cd $DIR && \
 echo "CryptoJS" && \
 svn checkout http://crypto-js.googlecode.com/svn/tags/3.1.2/ crypto-js-read-only && \
+wget https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.js && \
 echo "Done."
 
