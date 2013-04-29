3185f71d72a0c119ca75bbff7f198bd62faecc76
Binary <binary@jrudevels.org>
2013-04-29 13:10:16 +0700

    debug tools

---
+++ src/js/debug_tools.js
@@ -0,0 +1,17 @@
+debug_tools = {
+    getIDs: function(el) {
+        var r = [];
+        for (var i=0; i<el.childNodes.length; i++) {
+            r.push(el.childNodes[i].id + ' ' +  el.childNodes[i].getAttribute('removed'))
+        }
+        return r;
+    },
+    getElementById: function(parent, tagname, id) {
+        var els = parent.getElementsByTagName(tagname);
+        for (var i = 0; i<els.length; i++) {
+            if (els[i].id == id) {
+                return els[i];
+            }
+        }
+    }
+}
