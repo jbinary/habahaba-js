4e6b106fc4a4cd72dbbf7593ae5272b80c931f99
Binary <binary@jrudevels.org>
2013-06-19 20:35:02 +0700

    put features to the contact's presence

--- src/js/caps.js
+++ src/js/caps.js
@@ -1,14 +1,16 @@
 "use strict";
-require(['habahaba', 'jslix/caps'], function(habahaba, Caps) {
+require(['habahaba', 'jslix/caps', 'models'], function(habahaba, Caps, models) {
     var plugin = function(dispatcher, data, storage) {
             this._dispatcher = dispatcher;
             this._data = data;
             this._storage = storage;
         },
+        Model = models.Model,
         fields = {};
 
     fields.load = function() {
-        var disco_plugin = this._data.loaded_plugins.disco,
+        var loaded_plugins = this._data.loaded_plugins,
+            disco_plugin = loaded_plugins.disco,
             options = {
                 disco_plugin: disco_plugin.disco,
                 storage: this._storage,
@@ -16,10 +18,29 @@ require(['habahaba', 'jslix/caps'], function(habahaba, Caps) {
                 node: 'http://dev.habahaba.im/'
             };
         this.caps = this._dispatcher.registerPlugin(Caps, options);
+        this.caps.signals.caps_changed.add(this.capsChanged, this);
+        this.roster = loaded_plugins.roster;
+    }
+
+    fields.capsChanged = function(jid, features) {
+        var item = this.roster.get_roster_item(jid) || {},
+            presence;
+        $.each(item.presences || [], function() {
+            if (this.from.resource == jid.resource) {
+                presence = this;
+                return false;
+            }
+        });
+        if (presence !== undefined) {
+            presence.features = features;
+        debugger;
+            item.set();
+        }
     }
 
     fields.unload = function() {
         this._dispatcher.unregisterPlugin(this.caps);
+        // XXX: unregister signal handler
     }
 
     habahaba.Plugin({
