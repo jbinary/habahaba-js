22f9ff1c9abde2ec4721034779a04a6024ed2094
Binary <binary@jrudevels.org>
2014-01-16 00:52:45 +0700

    jingle dialogs are now draggable

--- src/js/view/view.js
+++ src/js/view/view.js
@@ -1,6 +1,7 @@
 "use strict";
 require(['habahaba', 'models', 'DOM-patch', 'libs/jquery',
-         'contextmenu/jquery.contextMenu', 'libs/jquery.transit'],
+         'contextmenu/jquery.contextMenu', 'libs/jquery.transit',
+         'view/draggable'],
         function(habahaba, models, DOM_patcher) {
     var roster_search_timer, Model, dispatcher, fields = {};
     var plugin = function(jslix_dispatcher, data, storage, account_storage) {
